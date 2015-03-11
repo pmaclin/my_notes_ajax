c88821c77a274a21b28755476cf0b21e326e1276
Raghu Betina <rbetina@github.com>
2015-03-11 15:55:30 -0500

    Ajaxifies Destroy action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -58,6 +58,7 @@ class NotesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 

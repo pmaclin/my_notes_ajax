c6cd9ed3bf4185b9429605d19b14248767d9c0a7
Raghu Betina <rbetina@github.com>
2015-03-11 16:25:49 -0500

    Ready for Create challenge

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -30,6 +30,7 @@ class NotesController < ApplicationController
       if @note.save
         format.html { redirect_to :back, notice: 'Note was successfully created.' }
         format.json { render :show, status: :created, location: @note }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @note.errors, status: :unprocessable_entity }
